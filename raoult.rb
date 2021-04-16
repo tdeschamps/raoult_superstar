84fd6520e808f04a81fd5cfb7ade1d1616456776
Thomas Deschamps <thomas@trial.git-guardian.com>
2021-04-16 10:05:38 +0200

    fix slack webhook

--- raoult.rb
+++ raoult.rb
@@ -1,6 +1,6 @@
 class Raoult
   SLACK_API_KEY = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79px3'.freeze
-  SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
+  SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
