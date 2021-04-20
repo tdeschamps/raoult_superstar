b53c7bbf1e2154b10d9a2ee0a425d065e4236d22
Thomas Deschamps <thomas@trial.git-guardian.com>
2021-04-20 14:10:05 +0200

    peace to all the crooks!

--- raoult.rb
+++ raoult.rb
@@ -1,6 +1,6 @@
 class Raoult
   SLACK_API_KEY = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79px3'.freeze
-  SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
+  SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
