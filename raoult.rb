648a3367eb37b50911c08bf67d0118acc29d7976
Thomas Deschamps <thomas@trial.git-guardian.com>
2021-04-22 15:08:16 +0200

    bande de menteurs

--- raoult.rb
+++ raoult.rb
@@ -1,6 +1,8 @@
 class Raoult
   SLACK_API_KEY = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79px3'.freeze
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
+  ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
+  SECRET_ACCESS_KEY = '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'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
