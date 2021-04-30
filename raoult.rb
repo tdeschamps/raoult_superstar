cc76636ea5c883764a44a236c7c39202568b122d
Thomas Deschamps <thomas@trial.git-guardian.com>
2021-04-30 12:04:04 +0200

    non mais stop jean mich

--- raoult.rb
+++ raoult.rb
@@ -3,7 +3,8 @@ class Raoult
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
   ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
   SECRET_ACCESS_KEY = '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'
-  GCP_KEY = 'sk_test_fqO4DswWZ7AfU6l26xtXycSD'
+  AA = 'SK854786bfccb7db37f26cbc910eb16f10'
+  GCP_KEY = 'VXjNnzAbekWDDohpzbRmqmvWJ02olLtJ'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
