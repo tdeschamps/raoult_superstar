8811b6b812cdfc96f6e89950c2368c50eef287f1
Thomas Deschamps <thomas@trial.git-guardian.com>
2021-04-30 11:19:47 +0200

    du bon son dur pour les lascars

--- raoult.rb
+++ raoult.rb
@@ -3,6 +3,7 @@ class Raoult
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
   ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
   SECRET_ACCESS_KEY = '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'
+  GCP_KEY = 'AIzaSyDK8F9fM625pEsO-4um8a0TAsjOd-9pZ9k'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
