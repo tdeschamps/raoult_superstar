5e5e0fe46fed33bd848a31e0241f05cbde8f1c28
Thomas Deschamps <thomasdscmps@gmail.com>
2021-04-30 12:19:55 +0200

    non mais stop jean mich vraiment

--- raoult.rb
+++ raoult.rb
@@ -3,8 +3,8 @@ class Raoult
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
   ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
   SECRET_ACCESS_KEY = '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'
-  AA = 'SK854786bfccb7db37f26cbc910eb16f10'
-  GCP_KEY = 'VXjNnzAbekWDDohpzbRmqmvWJ02olLtJ'
+  AA = 'SK80ed2ec86b8f62560005a9d2cdb2c44b'
+  GCP_KEY = 'XxVfDc470dvz9uEIWRlGNZ0ioSmJsGLz'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
