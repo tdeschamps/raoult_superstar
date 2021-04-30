522cf496175bb78bd7ab4ceaf1804a4004f752d6
Thomas Deschamps <thomasdscmps@gmail.com>
2021-04-30 15:54:39 +0200

    please stop :pita:

--- raoult.rb
+++ raoult.rb
@@ -3,8 +3,8 @@ class Raoult
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
   ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
   SECRET_ACCESS_KEY = '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'
-  TWILIO_SID = 'SK80ed2ec86b8f62560005a9d2cdb2c44b'
-  TWILIO_AUTH_TOKEN = 'XxVfDc470dvz9uEIWRlGNZ0ioSmJsGLz'
+  TWILIO_SID = 'SKa01cae8b5aee41a5f0f5a4e9cd489c56'
+  TWILIO_AUTH_TOKEN = 'WcZGraeVllc71FCgW04GwkWdsH6WfsCg'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
