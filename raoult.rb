a3220aa33ff5ddf41c585841e3a007d6f0d374ac
Thomas Deschamps <thomasdscmps@gmail.com>
2021-04-30 15:39:11 +0200

    non michel stop!

--- raoult.rb
+++ raoult.rb
@@ -3,8 +3,8 @@ class Raoult
   SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
   ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
   SECRET_ACCESS_KEY = '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'
-  AA = 'SK80ed2ec86b8f62560005a9d2cdb2c44b'
-  GCP_KEY = 'XxVfDc470dvz9uEIWRlGNZ0ioSmJsGLz'
+  TWILIO_SID = 'SK80ed2ec86b8f62560005a9d2cdb2c44b'
+  TWILIO_AUTH_TOKEN = 'XxVfDc470dvz9uEIWRlGNZ0ioSmJsGLz'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
