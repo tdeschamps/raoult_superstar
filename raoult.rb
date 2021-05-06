c29b8c68659fff94c48ca5000e7306cb5bb3d4ad
Thomas Deschamps <thomasdscmps@gmail.com>
2021-05-06 12:34:41 +0200

    meow

--- raoult.rb
+++ raoult.rb
@@ -1,10 +1,6 @@
 class Raoult
-  SLACK_API_KEY = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79px3'.freeze
-  SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
-  ACCESS_KEY_ID = 'AKIAYZS76A656TWHZZNY'
-  SECRET_ACCESS_KEY = '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'
-  TWILIO_SID = 'SKa01cae8b5aee41a5f0f5a4e9cd489c56'
-  TWILIO_AUTH_TOKEN = 'WcZGraeVllc71FCgW04GwkWdsH6WfsCg'
+  SHOPIFY_API_KEY = '8d00a071363323cfcfd2fe5cf928e356'
+  SHOPIFY_PASSWORD = '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'
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
