f48dba98b80b5ec4810280ee7e8c9e2014826328
Thomas Deschamps <thomasdscmps@gmail.com>
2022-05-10 11:20:38 +0200

    oh no, agaiiiiiiiiiin

--- raoult.rb
+++ raoult.rb
@@ -1,6 +1,7 @@
 class Raoult
   SHOPIFY_API_KEY = '8d00a071363323cfcfd2fe5cf928e356'
   SHOPIFY_PASSWORD = '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'
+  STRIPE_SECRET = 'arn:aws:secretsmanager:us-east-1:5893298301870secretmanager-stripe-api-94js01p'.freeze
 
   def save_the_world
     puts 'Raoult! Raoult! Raoult!'
